891e299ab9b747f8a7d3fa1c906027388c9b0b06
AlenaPliusnina <a564352@gmail.com>
2020-11-05 09:54:40 +0300

    update import in test_pytest.py

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,4 +1,13 @@
 import pytest
+
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
 from game import word_choice, check_letter, update_underscore
 
 
